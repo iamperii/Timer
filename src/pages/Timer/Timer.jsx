6f6b58976408016bc1f9ddf346538706664f0504
Memmedova Peri <perimemmedova20052022@gmail>
2024-12-22 00:59:22 +0400

    Timer Component states to redux.

--- src/pages/Timer/Timer.jsx
+++ src/pages/Timer/Timer.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
 import { FaPause, FaPlay } from 'react-icons/fa';
 import style from './Timer.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTime } from '../../redux/slices/timerSlice';
 
 const Timer = () => {
-	const [time, setTime] = useState({ minutes: 0, seconds: 0 });
+	const dispatch = useDispatch();
+	const time = useSelector((state) => state.timer.time);
 	const [isRunning, setIsRunning] = useState(false);
 	const [isEditable, setIsEditable] = useState(true);
 
@@ -16,42 +19,51 @@ const Timer = () => {
 		let timerId;
 		if (isRunning) {
 			timerId = setInterval(() => {
-				setTime((prevTime) => {
-					const totalSeconds = prevTime.minutes * 60 + prevTime.seconds;
-					if (totalSeconds <= 0) {
-						clearInterval(timerId);
-						setIsRunning(false);
-						setIsEditable(true);
-						alert('Timer bitdi!');
-						return { minutes: 0, seconds: 0 };
-					}
-
-					const newSeconds = (totalSeconds - 1) % 60;
-					const newMinutes = Math.floor((totalSeconds - 1) / 60);
-					return { minutes: newMinutes, seconds: newSeconds };
-				});
+				if (time.seconds > 0) {
+					dispatch(
+						setTime({
+							...time,
+							seconds: time.seconds - 1,
+						})
+					);
+				} else if (time.minutes > 0) {
+					dispatch(
+						setTime({
+							minutes: time.minutes - 1,
+							seconds: 59,
+						})
+					);
+				} else {
+					setIsRunning(false);
+					alert('Timer bitti!');
+					setIsEditable(true);
+				}
 			}, 1000);
 		}
 		return () => clearInterval(timerId);
-	}, [isRunning]);
+	}, [isRunning, time]);
 
-	const incrementTime = (time) => {
+	const incrementTime = (type) => {
 		if (!isEditable) return;
-		setTime((prevTime) => ({
-			...prevTime,
-			[time]: prevTime[time] + 1,
-		}));
+		dispatch(
+			setTime({
+				...time,
+				[type]: time[type] + 1,
+			})
+		);
 	};
 
-	const decrementTime = (time) => {
+	const decrementTime = (type) => {
 		if (!isEditable) return;
-		setTime((prevTime) => ({
-			...prevTime,
-			[time]: Math.max(0, prevTime[time] - 1),
-		}));
+		dispatch(
+			setTime({
+				...time,
+				[type]: time[type] > 0 ? time[type] - 1 : 0,
+			})
+		);
 	};
 
-	const formatTime = (time) => (time < 10 ? `0${time}` : time);
+	const formatTime = (value) => (value < 10 ? `0${value}` : value);
 
 	return (
 		<div className={style.timer}>
